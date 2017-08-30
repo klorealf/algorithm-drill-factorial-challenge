0624bd132d923470da6b45bb026a2af019afbcf5
Andres Arias & Thierry Moudiki <pair+aarias89.thierrymoudiki@devbootcamp.com>
2017-08-30 10:17:41 -0400

    implement factorial iterative and recursive

--- factorial.rb
+++ factorial.rb
@@ -1,5 +1,20 @@
+# def factorial_iterative(n)
+#   return 1 if n <= 1
+
+#   prod = 1
+#   (1..n).each do |elm|
+#     prod = prod * elm
+#   end
+
+#   return prod
+
+# end
+
 def factorial_iterative(n)
+  (1..n).reduce(1) {|prod, elm| prod * elm}
 end
 
 def factorial_recursive(n)
+  return 1 if n <= 1
+    n * factorial_recursive(n-1)
 end
