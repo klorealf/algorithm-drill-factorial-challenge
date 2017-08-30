7eebd6a07d92af1cb34a361cb8affc961072e3d3
bradsloan & Andreas Bach Landgrebe <pair+bsloan3.grebes15@devbootcamp.com>
2017-08-30 10:17:12 -0400

    add factorial iterative and recursive

--- factorial.rb
+++ factorial.rb
@@ -1,5 +1,16 @@
 def factorial_iterative(n)
+  sum = 1
+  until n == 0
+    sum *= n
+    n -= 1
+  end
+  sum
 end
 
 def factorial_recursive(n)
+  if n == 0
+    return 1
+  else
+    return n * factorial_recursive(n-1)
+  end
 end
