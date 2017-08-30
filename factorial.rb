1f7bbf6fcbb63d2cdbaa306e8032da48a8418b24
Amber Maillard & klorealf & Samory Harris <pair+amber2394.klorealf.samory070@devbootcamp.com>
2017-08-30 10:29:20 -0400

    add factorial iterative method to complete release 0

--- factorial.rb
+++ factorial.rb
@@ -1,5 +1,16 @@
 def factorial_iterative(n)
+  if n <= 1
+    return 1
+  else
+    count = 1
+    while n>1
+    count *= n
+    n -=1
+    end
+  end
+  count
 end
+
 
 def factorial_recursive(n)
 end
