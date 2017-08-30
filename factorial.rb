f048cd3eee28d34dd1e346b1a506972aca924cfd
Catherine Rice & David Skaggs <pair+clr2107.davidtskaggs@devbootcamp.com>
2017-08-30 10:47:49 -0400

    Add recursive factorial method

--- factorial.rb
+++ factorial.rb
@@ -1,5 +1,18 @@
-def factorial_iterative(n)
+def factorial_iterative(n) #10
+  if n == 0
+    return 1
+  elsif
+    counter = n - 1
+    product = n
+    while counter > 0
+      product *= counter
+      counter -=1
+    end
+  end
+return product
 end
 
 def factorial_recursive(n)
+  return 1 if n == 0
+    n * factorial_recursive(n-1)
 end
