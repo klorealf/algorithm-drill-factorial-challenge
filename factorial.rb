fb9fb8b980c0d3328c3a3b370fbea411a952a758
Chris Camp <cpcamp2@gmail.com>
2017-09-09 15:49:20 -0400

    Complete release 2

--- factorial.rb
+++ factorial.rb
@@ -1,5 +1,19 @@
 def factorial_iterative(n)
+  number = n
+  current_solution = 1
+  while number > 0
+    current_solution *= number
+    number -= 1
+  end
+  current_solution
 end
 
+
+
 def factorial_recursive(n)
+  if n == 0
+    1
+  else
+    n * factorial_recursive(n-1)
+  end
 end
